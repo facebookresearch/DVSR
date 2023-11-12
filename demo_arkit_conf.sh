46570ee8d6de0d9e25661041b50c46d92427253d
zhsun0357 <zhsun@stanford.edu>
2023-11-12 23:28:32 +0000

    arkit real-world eval

--- demo_arkit_conf.sh
+++ demo_arkit_conf.sh
@@ -1,1 +1,1 @@
-python video_demo.py configs/arkit_config.py chkpts/dvsr_tartan.pth /home/szhbchr/DVSR/data/arkit_3_long /home/szhbchr/DVSR/results/demo_arkit --device 1
+python video_demo.py configs/arkit_config.py chkpts/dvsr_tartan.pth /home/szhbchr/DVSR/data/arkit_3_long /home/szhbchr/DVSR/results/demo_arkit_3 --device 1
